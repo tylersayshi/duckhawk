254070f64ad7e5cf743be51e9b79931ee08cae41
Tyler <26290074+thegitduck@users.noreply.github.com>
2024-12-21 17:29:47 -0800

    0.1.1 fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import { allChunked } from "./all-chunked.ts";
 import { map } from "./map.ts";
 import { mapSeries } from "./map-series.ts";
 import { props } from "./props.ts";
+import { reduce } from "./reduce.ts";
 
-export default { map, mapSeries, props };
+export default { allChunked, map, mapSeries, props, reduce };
